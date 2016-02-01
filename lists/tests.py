de5fe842090a2947bd5367d3d2756c4bb103f6ef
Nick Arnold <nick.arnold@dev.local>
2016-02-01 14:24:14 -0500

    Basic view now returns minimal HTML

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from lists.views import home_page
+from django.http import HttpRequest
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -8,4 +9,11 @@ class HomePageTest(TestCase):
     def test_root_url_resolves(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do Lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
 
