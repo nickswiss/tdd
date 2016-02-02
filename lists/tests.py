1e366c3b39f9d064fb720cc87d9c3ce3a544121c
Nick Arnold <nick.arnold@dev.local>
2016-02-02 10:51:02 -0500

    Updates table with single entered item

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,7 @@ from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
+
 # Create your tests here.
 class HomePageTest(TestCase):
 
@@ -14,6 +15,18 @@ class HomePageTest(TestCase):
     def test_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': 'A new list item'},
+            request=request
+        )
         self.assertEqual(response.content.decode(), expected_html)
 
+    def test_home_page_can_save_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertIn('A new list item', response.content.decode())
