9212faf6802fcfa1aa725dd0f57a877af148ad5e
Nick Arnold <nick.arnold@dev.local>
2016-02-09 15:46:29 -0500

    Improved layout using bootstrap and refactored templates to use a base template

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,13 +3,14 @@
 # Nick Arnold
 #
 from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from unittest import TestCase
 import unittest
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
