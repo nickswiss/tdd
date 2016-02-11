26b55db33d255af60f9274cfc8c60144ca924168
Nick Arnold <nick.arnold@dev.local>
2016-02-11 17:03:40 -0500

    Implemented list item as ModelForm and tests

--- lists/models.py
+++ lists/models.py
@@ -1,9 +1,14 @@
 from __future__ import unicode_literals
-
+from django.core.urlresolvers import reverse
 from django.db import models
 
+
+
 class List(models.Model):
-    pass
+
+    def get_absolute_url(self):
+        return reverse('view_list', kwargs={'list_id': self.id})
+
 
 class Item(models.Model):
     text = models.TextField(default='')
--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,5 @@
 from django.core.exceptions import ValidationError
 from django.core.urlresolvers import reverse
-from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from models import Item, List
 
@@ -20,14 +19,21 @@ def new_list(request):
         error = "You can't have an empty list item"
         return render(request, 'home.html', {'error': error})
 
-    path = reverse('view_list', kwargs={'list_id': list_.id})
-    return redirect(path)
+    # path = reverse('view_list', kwargs={'list_id': list_.id})
+    # return redirect(path)
+    return redirect(list_)
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
+    error = None
 
     if request.method == 'POST':
-        Item.objects.create(text=request.POST.get('item_text'), list=list_)
-        return redirect(reverse('view_list', kwargs={'list_id': list_.id}))
+        try:
+            item = Item(text=request.POST['item_text'], list=list_)
+            item.full_clean()
+            item.save()
+            return redirect(list_)
+        except ValidationError:
+            error = "You can't have an empty list item"
 
-    return render(request, 'list.html', {'list': list_})
+    return render(request, 'list.html', {'list': list_, 'error': error})
